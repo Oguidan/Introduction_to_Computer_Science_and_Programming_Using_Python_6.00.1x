ce2176046c0595e9152331af5ba9b622b1a60381
Michael Oguidan <michaeloguidan@gmail.com>
2024-02-13 10:23:08 +0000

    Update function to handle edge cases

---
+++ type.py
@@ -0,0 +1,44 @@
+# The following python code is a simple example, and perfect example of being execute in the python interpreter.
+
+3.5
+# Output: 3.5
+
+type(3.5)
+# Output: <class 'float'>
+
+5
+# Output: 5
+
+type(5)
+# Output: <class 'int'>
+
+float(5)
+# Output: 5.0
+
+int(3.9)
+# Output: 3
+
+3 + 2
+# Output: 5
+
+print(3 + 2)
+# Output: 5
+
+3.0 + 2
+# Output: 5.0
+
+5 / 2
+# Output: 2.5
+
+5 // 2
+# Output: 2
+
+5 % 2
+# Output: 1
+
+3 * 5 + 1
+# Output: 16
+
+
+3 * (5 + 1)
+# Output: 18
