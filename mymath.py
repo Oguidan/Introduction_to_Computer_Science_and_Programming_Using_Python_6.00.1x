de34d5552ead982c9026e5e70984b9a0d538710e
Michael Archangel <michaeloguidan@gmail.com>
2024-06-29 21:14:38 +0000

    define my math library

---
+++ mymath.py
@@ -0,0 +1,21 @@
+def inc(num):
+    '''
+    (numeric) -> numeric
+
+    Returns the number obtained
+    after adding 1 to the given
+    number.
+    '''
+
+    return num + 1
+
+def dec(num):
+    '''
+    (numeric) -> numeric
+    
+    Return the number obtained
+    after subtracting 1 from the
+    given number.
+    '''
+
+    return num - 1
